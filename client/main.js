7c713f27d24a35fdba65a78dd3019080ebbc20b3
Carlos Gabriel <gabrielggff25@gmail.com>
2019-04-30 13:21:53 -0300

    Added card animations

--- client/main.js
+++ client/main.js
@@ -34,3 +34,53 @@ App.controller("GraphCtrl", function ($scope) {
         console.log(points, evt);
     };
 });
+
+/**
+ * Add animated in html element on mouse over
+ * @param {*} event 
+ */
+function onOver(event) {
+    var nameAnimate = "heartBeat";
+    this.animateCSS(event.target, nameAnimate);
+}
+
+function animateCSS(element, animationName, callback) {
+    this.addClassAnimatedAndAnotherClass(element,animationName);
+    if (typeof callback === 'function') callback()
+    function handleAnimationEnd() {
+        var className = 'animated';
+        element.classList.remove(className, animationName)
+        this.removeElementHandler(element, handleAnimationEnd);
+    }
+    this.addHandlerToElementAnimationEnd(element, handleAnimationEnd);
+}
+
+
+//Adcionando função handler no evento ao final da animação
+function addHandlerToElementAnimationEnd(element,handleAnimationEnd){ 
+    var nameEvent = "animationend";
+    element.addEventListener(nameEvent, handleAnimationEnd)
+}
+//Adcionando função handler no evento ao começo da animação
+function addHandlerToElementAnimationStart(element,handleAnimationStart){
+    var nameEvent = "animationstart";
+    element.addEventListener(nameEvent, handleAnimationStart)
+}
+
+function removeElementHandler(element, handler) {
+    var nameAnimation = "animationend";
+    element.removeEventListener(nameAnimation, handler)
+}
+
+function addClassAnimatedAndAnotherClass(element, anotherClass) {
+    this.addClassAnimatedInElement(element);
+    this.addClassInElement(element, anotherClass);
+}
+
+function addClassAnimatedInElement(element) {
+    this.addClassInElement(element, 'animated');
+}
+
+function addClassInElement(element, clazz) {
+    element.classList.add(clazz);
+}
