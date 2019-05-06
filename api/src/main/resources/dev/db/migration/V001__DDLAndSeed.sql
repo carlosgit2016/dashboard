14553b27dc58e3aba398d231a5d47393e4d57e63
Daniel Junior Silva <danjun48@gmail.com>
2019-05-06 15:15:07 -0300

    profiles

--- api/src/main/resources/dev/db/migration/V001__DDLAndSeed.sql
+++ api/src/main/resources/dev/db/migration/V001__DDLAndSeed.sql
@@ -1,3 +1,39 @@
+CREATE TABLE minerio(
+	id_minerio BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+	nome varchar(60) NOT NULL,
+	valor_grama DECIMAL(10, 6) NOT NULL
+)engine=InnoDB default charset=utf8;
+
+CREATE TABLE tipo_veiculo(
+	id_tipo_veiculo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+	nome varchar(20) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE cliente(
+	id_cliente BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(80) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE veiculo(
+	id_veiculo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+	id_tipo_veiculo BIGINT(20),
+	FOREIGN KEY (id_tipo_veiculo) REFERENCES tipo_veiculo(id_tipo_veiculo)
+)ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+CREATE TABLE pedido(
+	id_pedido BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+	id_veiculo BIGINT(20) NOT NULL,
+	id_cliente BIGINT(20) NOT NULL,
+	id_minerio BIGINT(20) NOT NULL,
+	destino varchar(150) NOT NULL,
+	quantidade_kg decimal(15,2) NOT NULL,
+	data_pedido TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY (id_veiculo) REFERENCES veiculo(id_veiculo),
+	FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+	FOREIGN KEY (id_minerio) REFERENCES minerio(id_minerio)
+)ENGINE =InnoDB DEFAULT CHARSET=UTF8;
+
+
 INSERT INTO cliente (nome) VALUES ("Daniel Junior");
 INSERT INTO cliente (nome) VALUES ("Petrobrás");
 INSERT INTO cliente (nome) VALUES ("Pedreira Guaravera");
@@ -96,4 +132,8 @@ INSERT INTO pedido (id_veiculo, id_cliente, id_minerio, destino, quantidade_kg) 
 
 INSERT INTO pedido (id_veiculo, id_cliente, id_minerio, destino, quantidade_kg) VALUES 
 (19, 3, 10, "SP/SP 101", 51);
+
+
+
+
 
